7c757b06b3ca617fb06514be0daffa4d3116c936
VeRRuckTTe <123332419+VeRRuckTTe@users.noreply.github.com>
2024-09-29 23:11:18 +0300

    Add files via upload

---
+++ PR_4/Program.cs
@@ -0,0 +1,38 @@
+﻿//**************************************************
+//*Практическая работа № 1                         *
+//*Выполнила: Корнеева В.Е., группа 2ИСП           *
+//*Задание: Составить программу линейного алгоритма*
+//**************************************************
+using System;
+
+namespace PR_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Практическая работа №4";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Здраствуйте!");
+            Console.Write("Введите число a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число b: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите число c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double t1, t2, t3, t4, t5, t6, t7, t8, t9,t10, s;
+            t1 = Math.Pow(10, 1/3);
+            t2 = Math.Pow(a, 3 * Math.Abs(b));
+            t3 = 1 / t2;
+            t4 = Math.Pow(b, 4);
+            t5 = Math.Pow(c, 1/2);
+            t6 = t3 * t4 * t5;
+            t8 = t1 + t6;
+            t9 = Math.Log10(t8);
+            t10 = Math.Pow(Math.E, a * b * Math.Pow(c, -2));
+            s = t9 + t10;
+            Console.WriteLine("Результат: " + Math.Round(s, 4));
+            Console.ReadKey();
+        }
+    }
+}
---
+++ PR_7/Program.cs
@@ -0,0 +1,83 @@
+﻿/******************************************************************************/
+/* Практическая работа №8                                                     */
+/* Выполнила: Корнеева В.Е., 2-ИСП                                            */
+/* Задание: Составить программу для вычисления функций                        */
+/******************************************************************************/
+using System;
+
+namespace PR_7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.Title = "Практическая работа №7";
+            Console.WriteLine("Здравствуйте.");
+            int a = 0, b = 0, h;
+            double result;
+            for (; ; )
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write("Введите a: ");
+                    a = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите b: ");
+                    b = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Введите h: ");
+                    h = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (FormatException fEx)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ошибка: {fEx.Message}");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Ошибка: {ex.Message}");
+                    Console.ReadKey();
+                    Console.Clear();
+                    continue;
+                }
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                if (a < b)
+                {
+                    for (double i = a; i <= b; i += h)
+                    {
+                        result = 1 / Math.Tan(i / 3) + 0.5 * Math.Sin(i);
+                        Console.WriteLine($"Значение аргумента: {i}, значение функции: {Math.Round(result, 5)}");
+                    }
+                }
+                else
+                {
+                    for (double i = a; i >= b; i += h)
+                    {
+                        if (h < 0)
+                        {
+                            result = 1 / Math.Tan(i / 3) + 0.5 * Math.Sin(i);
+                            Console.WriteLine($"Значение аргумента: {i}, значение функции: {Math.Round(result, 5)}");
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("Упс... Ошибка, неправильно введены значения");
+                            Console.ReadKey();
+                            break;
+                        }
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.Green;   
+                Console.Write("Вы хотите выйти? Нажмите Y, иначе любую другую клавишу");
+                if (Console.ReadKey().Key == ConsoleKey.Y)
+                    break;
+                Console.Clear();
+            }
+        }
+    }
+}
+
